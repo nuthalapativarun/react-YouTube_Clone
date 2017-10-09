09630f5c7406567496299784cd5af3fc18b6e855
Varun Nuthalapati <varun.nuthalapati@trinet.com>
2017-10-08 22:30:31 -0700

    Final push

---
+++ src/index.js
@@ -0,0 +1,50 @@
+import _ from 'lodash';
+import React, { Component } from 'react';
+import ReactDom from 'react-dom';
+
+import YTSearch from 'youtube-api-search';
+
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list.js';
+import VideoDetail from './components/video_detail';
+
+const API_KEY = 'AIzaSyCVdi3yKf8Oyy3eyDu0mLdooNMlWVs7CAo';
+
+
+
+class App extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = { 
+            videos: [],
+            selectedVideo: null
+        };
+
+        this.videoSearch('New York');
+        
+    }
+
+    videoSearch(term) {
+            YTSearch({ key: API_KEY, term: term },(videos) => {
+                this.setState({
+                    videos: videos,
+                    selectedVideo: videos[0]
+                });
+            });
+    }
+
+    render() {
+
+        const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
+        return (
+            <div>
+                <SearchBar onVideoSearch={videoSearch} />
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList onVideoSelect={selectedVideo => this.setState({selectedVideo})} videos={this.state.videos}/>
+            </div>
+        );
+    }
+}
+
+ReactDom.render(<App />, document.querySelector('.container'));
